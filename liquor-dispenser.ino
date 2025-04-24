4b4b6a1fbeadf1ae2a0fed061068d3ad897a6851
QadamosssQ <altadamoss@gmail.com>
2025-04-24 08:37:49 +0200

    Added main code v1

---
+++ liquor-dispenser.ino
@@ -0,0 +1,40 @@
+#define liqid1 1
+#define liqid2 2
+#define liqid3 3
+#define liqid4 4
+
+#define liqid1btn 5
+#define liqid2btn 6
+#define liqid3btn 7
+#define liqid4btn 8
+
+void setup() {
+  pinMode(liqid1btn, INPUT_PULLUP);
+  pinMode(liqid2btn, INPUT_PULLUP);
+  pinMode(liqid3btn, INPUT_PULLUP);
+  pinMode(liqid4btn, INPUT_PULLUP);
+
+  pinMode(liqid1, OUTPUT);
+  pinMode(liqid2, OUTPUT);
+  pinMode(liqid3, OUTPUT);
+  pinMode(liqid4, OUTPUT);
+}
+
+void loop(){
+    while(digitalRead(liqid1btn) == LOW){
+      digitalWrite(liqid1, HIGH);
+    }
+    while(digitalRead(liqid2btn) == LOW){
+      digitalWrite(liqid2, HIGH);
+    }
+    while(digitalRead(liqid3btn) == LOW){
+      digitalWrite(liqid3, HIGH);
+    }
+    while(digitalRead(liqid4btn) == LOW){
+      digitalWrite(liqid4, HIGH);
+    }
+    digitalWrite(liqid1, LOW);
+    digitalWrite(liqid2, LOW);
+    digitalWrite(liqid3, LOW);
+    digitalWrite(liqid4, LOW);
+}
